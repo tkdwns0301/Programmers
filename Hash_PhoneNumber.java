5cbbf735e5234dcccf05969dc0e20ff8324ed85c
hand <tkdwns0301@gmail.com>
2021-01-07 00:18:54 +0900

    Hash_PhoneNumber
    2021.01.07

---
+++ Hash_PhoneNumber.java
@@ -0,0 +1,19 @@
+package Hash;
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+		
+		Arrays.sort(phone_book);
+		
+		for (int i = 0; i < phone_book.length-1; i++) {
+			if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+		}
+
+		return answer;
+    }
+}
