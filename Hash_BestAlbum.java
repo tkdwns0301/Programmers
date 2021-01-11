607c44af6ef87eea631a37cdeecc611acc7e5b92
hand <tkdwns0301@gmail.com>
2021-01-11 22:06:22 +0900

    Hash_BestAlbum
    2021.01.11

--- Hash_BestAlbum.java
+++ Hash_BestAlbum.java
@@ -2,56 +2,50 @@ package Hash;
 import java.util.*;
 
 class Solution {
-	public int[] solution(String[] genres, int[] plays) {
-		Vector<Integer> answer2 = new Vector<Integer>();
-		HashMap<Integer, String> genre = new HashMap<Integer, String>();
-		HashMap<Integer, Integer> play = new HashMap<Integer, Integer>();
-		Map<String, Integer> sumPlay = new HashMap<String, Integer>();
-
-		for (int i = 0; i < genres.length; i++) {
-			String a = genres[i];
-			sumPlay.put(genres[i], sumPlay.getOrDefault(a, 0) + plays[i]);
-			genre.put(i, genres[i]);
-			play.put(i, plays[i]);
-		}
-
-		sumPlay = sortStringMapByValue(sumPlay);
-		play = sortIntMapByValue(play);
-
-		int cnt = 0;
-
-		for (Map.Entry<String, Integer> entrySumPlay : sumPlay.entrySet()) {
-			Vector<Integer> temp = new Vector<Integer>();
-			for (Map.Entry<Integer, Integer> entryPlay : play.entrySet()) {
-				
-				if (entrySumPlay.getKey().equals(genre.get(entryPlay.getKey()))) {
-					temp.add(entryPlay.getKey());
-				}
+    public int[] solution(String[] genres, int[] plays) {
+            Vector<Integer> answer2 = new Vector<Integer>();
+			HashMap<Integer, String> genre = new HashMap<Integer, String>();
+			HashMap<Integer, Integer> play = new HashMap<Integer, Integer>();
+			Map<String, Integer> sumPlay = new HashMap<String, Integer>();
+			
+			for (int i = 0; i < genres.length; i++) {
+				String a = genres[i];
+				sumPlay.put(genres[i], sumPlay.getOrDefault(a, 0) + plays[i]);
+				genre.put(i, genres[i]);
+				play.put(i, plays[i]);
 			}
 			
-			for(int i=0; i<temp.size(); i++) {
-				if(i > 1)
-					break;
-				answer2.add(temp.get(i));
+			sumPlay = sortStringMapByValue(sumPlay);
+			play = sortIntMapByValue(play);
+
+			for (Map.Entry<String, Integer> entrySumPlay : sumPlay.entrySet()) {
+					Vector<Integer> temp = new Vector<Integer>();
+					for (Map.Entry<Integer, Integer> entryPlay : play.entrySet()) {
+						if (entrySumPlay.getKey().equals(genre.get(entryPlay.getKey()))) {
+							temp.add(entryPlay.getKey());
+						}
+					}
+					
+					for(int i=0; i<temp.size(); i++) {
+						if(i > 1)
+							break;
+						answer2.add(temp.get(i));
+					}
 			}
 
-			cnt++;
-	}
+			int[] answer = new int[answer2.size()];
+			
+			for(int i=0; i<answer2.size(); i++) {
+				answer[i] = answer2.get(i);
+			}
 
-		int[] answer = new int[answer2.size()];
-		
-		for(int i=0; i<answer2.size(); i++) {
-			answer[i] = answer2.get(i);
-		}
-
-		return answer;
-	}
-
-	public static LinkedHashMap<Integer, Integer> sortIntMapByValue(Map<Integer, Integer> album) {
+			return answer;
+    }
+    public static LinkedHashMap<Integer, Integer> sortIntMapByValue(Map<Integer, Integer> album) {
 		List<Map.Entry<Integer, Integer>> entries = new LinkedList<>(album.entrySet());
 		Collections.sort(entries, (o2, o1) -> o1.getValue().compareTo(o2.getValue()));
-
-		LinkedHashMap<Integer, Integer> result = new LinkedHashMap<>();
+			
+        LinkedHashMap<Integer, Integer> result = new LinkedHashMap<>();
 		for (Map.Entry<Integer, Integer> entry : entries) {
 			result.put(entry.getKey(), entry.getValue());
 		}
@@ -63,12 +57,11 @@ class Solution {
 		List<Map.Entry<String, Integer>> entries = new LinkedList<>(album.entrySet());
 		Collections.sort(entries, (o2, o1) -> o1.getValue().compareTo(o2.getValue()));
 
-		LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+        LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
 		for (Map.Entry<String, Integer> entry : entries) {
 			result.put(entry.getKey(), entry.getValue());
 		}
 
-		return result;
+        return result;
 	}
-
 }
